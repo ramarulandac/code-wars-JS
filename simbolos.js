218fecabc9f2e89be36b72ac41fb62ba373c352a
rené <rene.marulanda@gmail.com>
2019-12-17 22:25:48 +0100

    2nd Assigment Simbolos - Arab to Roman number.

--- simbolos.js
+++ simbolos.js
@@ -1,41 +1,9 @@
 
-/**************************************************************
- Romano | Árabe 
---------|-------
- I | 1 
- V | 5 
- X | 10 
- L | 50 
- C | 100 
- D | 500 
- M | 1000
- 
- 
-### Reglas
-
-Sólo se contemplan números entre el 1 y el 3999
-
-* Los símbolos I, X, C y M se pueden repetir hasta tres veces.
-* Los símbolos I, X y C se suman si están a la derecha de otro mayor o igual.
-* Los símbolos I, X y C se restan si están a la izquierda de otro mayor y solamente pueden anteponerse a los dos símbolos que le siguen en la sucesión.
-* I se resta de V y X
-* X se resta de L y C
-* C se resta de D y M
-* Los símbolos V, L y D no pueden repetirse.
-* Los símbolos V, L y D no pueden colocarse a la izquierda de otro mayor.
-
-### Ejercicios
-
-* Crear una función para pasar de número romanos a árabes
-* Crear una función para pasar de árabes a romanos
-* Hacer un validador de números romanos **********************/
-
-
+// Roman number validator
 
 const romanNumberValidator = (romanNumbers) => {
 
     let romanDictionary = {
-
         'I':1,
         'V':5,
         'X':10,
@@ -62,7 +30,6 @@ const romanNumberValidator = (romanNumbers) => {
         tokenValue = romanDictionary[token]   
 
         i++; // go ahead to next token
-
         nextToken = romanArrayNumber[i]
         nextTokenValue = romanDictionary[nextToken]      
         
@@ -76,17 +43,36 @@ const romanNumberValidator = (romanNumbers) => {
             valid = true;
         // Fewer Values at right    
         } else if (tokenValue < nextTokenValue && ((nextToken ==='V' && token === 'I') ||(nextToken ==='X' && token === 'I') ||
-                                                            (nextToken ==='L' && token === 'X') ||(nextToken ==='C' && token === 'X')||
-                                                            (nextToken ==='M' && token === 'C') ||(nextToken ==='D' && token === 'C'))){
+                                                   (nextToken ==='L' && token === 'X') ||(nextToken ==='C' && token === 'X')||
+                                                   (nextToken ==='M' && token === 'C') ||(nextToken ==='D' && token === 'C'))){
             valid = true;
         }    
 
-     // Out of tokens or valid false? get the hell out of here
-     //otherwise stay    
+     // Out of tokens or valid false? get the hell out of here, otherwise stay    
     } while(i < romanArrayNumber.length -1 && valid == true) 
   
     return valid
 }
 
-console.log(romanNumberValidator('MV'))
-console.log(romanNumberValidator('MMDCDLXIX'))
+// Arab to roman number
+function arabToRomanNumber(number) {
+
+    if (typeof number != 'number' && number <= 0 || number >= 3999) throw Error(`${number} data type or integer value is between the range from 1 - 3999 `);
+
+    let base = [1000,100,10,1] // base to split the numbers
+    let Thousands = {0:'',1:'M',2:'MM',3:'MMM'}
+    let Hundreds = {0:'', 1:'C', 2:'CC', 3:'CCC', 4:'CD', 5:'D', 6:'DC', 7:'DCC', 8:'DCCC', 9:'CM' }
+    let Tens = {0:'', 1:'X',2:'XX',3:'XXX', 4:'XL', 5:'L', 6:'LX', 7:'LXX', 8:'LXXX', 9:'XC'}
+    let Units = {0:'', 1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX'}
+
+    let romanNumberArrayIndex = [] //Index to build the roman number searching on the dicts.
+    let numberAux = number    
+    
+    base.forEach(x => { romanNumberArrayIndex.push(Math.trunc(numberAux/x)); // Splitting  decimal system numbers   
+                        numberAux =  Math.trunc(numberAux%x)})    
+
+    return Thousands[romanNumberArrayIndex[0]] + Hundreds[romanNumberArrayIndex[1]] + Tens[romanNumberArrayIndex[2]] + Units[romanNumberArrayIndex[3]]
+
+}
+
+console.log(arabToRomanNumber(3004))
